e6adff529722c064c9b483d0a1756eda2aed424d
Henry Shaw <103895027+TheOwl740@users.noreply.github.com>
2023-10-24 08:46:12 -0400

    V0.1
    Fixed version control

--- startup.js
+++ startup.js
@@ -1,16 +1,20 @@
 //jshint maxerr: 10000
 
 //VERSION CONTROL
-const version = 0;
+const version = 3;
 const versionData = [
-  "V0: Welcome to Goose Sandbox version 0:  this version is before my engine rework and is the original release. You will get an alert like this on startup every time there is a new version of the game."
+  "Version 0: Welcome to Goose Sandbox version 0:  this version is before my engine rework and is the original release. You will get an alert like this on startup every time there is a new version of the game."
 ];
+if(localStorage.getItem("version") === null) {
+  localStorage.setItem("version", -1);
+}
 if(version != localStorage.getItem("version")) {
   let ret = "";
+  let updates = version - Number(localStorage.getItem("version"));
   for(u = Number(localStorage.getItem("version")); u < version; u++) {
     ret += "\n \n" + versionData[u + 1];
   }
-  alert(localStorage.getItem("version") !== null ? "There have been " + (version - Number(localStorage.getItem("version"))) + " updates since you last visited Goose Sandbox: " + ret : versionData[0]);
+  alert("There " + (updates > 1 ? "have" : "has") + " been " + updates + " " + (updates > 1 ? "updates" : "update") + " since you last visited Goose Sandbox: " + ret);
   localStorage.setItem("version", version);
 }
 
