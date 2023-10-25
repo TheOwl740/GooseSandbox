0c92855c38df50c85f54b886ce81b285979235a1
Henry Shaw <103895027+TheOwl740@users.noreply.github.com>
2023-10-25 16:39:37 -0400

    V1

--- version.js
+++ version.js
@@ -1,9 +1,11 @@
 //jshint maxerr: 10000
 
 //VERSION CONTROL
-const version = 0;
+inApp = false;
+const version = 1;
 const versionData = [
-  "Version 0: Welcome to Goose Sandbox version 0:  this version is before my engine rework and is the original release. You will get an alert like this on startup every time there is a new version of the game."
+  "Version 0: Welcome to Goose Sandbox version 0:  this version is before my engine rework and is the original release. You will get an alert like this on startup every time there is a new version of the game.",
+  "V1: Version 1! this edition is updated to use my the newest version of my JS game engine. It removes the bodies of water temporarily (will reapply in the future) for performance, replacing them with various rocks and grasses to add character to the landscape. HUD also improved with more apparent controls."
 ];
 if(localStorage.getItem("version") === null) {
   localStorage.setItem("version", -1);
