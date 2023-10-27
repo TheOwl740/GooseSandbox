81daba12675a1ac8c5a637eedcd8a5c429c2190c
Henry Shaw <103895027+TheOwl740@users.noreply.github.com>
2023-10-27 00:11:37 -0400

    V2.1
    desc fix

--- version.js
+++ version.js
@@ -6,7 +6,7 @@ const version = 2;
 const versionData = [
   "Version 0: Welcome to Goose Sandbox version 0:  this version is before my engine rework and is the original release. You will get an alert like this on startup every time there is a new version of the game.",
   "V1: Version 1! this edition is updated to use my the newest version of my JS game engine. It removes the bodies of water temporarily (will reapply in the future) for performance, replacing them with various rocks and grasses to add character to the landscape. HUD also improved with more apparent controls.",
-  "V2: Changed format to RPG type. Use WASD to control your goose and walk around the map. Fixed issue with geese not walking around automatically, and fixed issue where baby geese can lay eggs."
+  "V2: Changed format to RPG type. Use WASD to control your goose and walk around the map. Fixed issue with geese not walking around automatically, and fixed issue where baby geese can lay eggs. Also improved rendering with dedicated render pipline."
 ];
 if(localStorage.getItem("version") === null) {
   localStorage.setItem("version", -1);
